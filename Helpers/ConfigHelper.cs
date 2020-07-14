c664a63c3a01d14112526206bc6874a856571226
Il Harper <afanyiyu@hotmail.com>
2020-07-14 18:59:47 +0800

    feat: Add RuminoidProductAttribute

--- Helpers/ConfigHelper.cs
+++ Helpers/ConfigHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -19,11 +20,40 @@ namespace Ruminoid.Common.Helpers
             return folder;
         }
 
-        public static T OpenConfig(string productName) =>
-            JsonConvert.DeserializeObject<T>(File.OpenText(Path.Combine(GetConfigFolder(productName), "config.json"))
+        public static T OpenConfig()
+        {
+            RuminoidProductAttribute product =
+                (RuminoidProductAttribute)Attribute.GetCustomAttribute(typeof(T), typeof(RuminoidProductAttribute));
+            if (product == null) throw new CustomAttributeFormatException("Product name not found.");
+            return JsonConvert.DeserializeObject<T>(File
+                .OpenText(Path.Combine(GetConfigFolder(product.ProductName), "config.json"))
                 .ReadToEnd());
+        }
 
-        public static void SaveConfig(string productName, T config) => File.WriteAllText(
-            Path.Combine(GetConfigFolder(productName), "config.json"), JsonConvert.SerializeObject(config));
+        public static void SaveConfig(T config)
+        {
+            RuminoidProductAttribute product =
+                (RuminoidProductAttribute) Attribute.GetCustomAttribute(typeof(T), typeof(RuminoidProductAttribute));
+            if (product == null) throw new CustomAttributeFormatException("Product name not found.");
+            File.WriteAllText(
+                Path.Combine(GetConfigFolder(product.ProductName), "config.json"), JsonConvert.SerializeObject(config));
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    sealed class RuminoidProductAttribute : Attribute
+    {
+        public RuminoidProductAttribute(string productName)
+        {
+            _productName = productName;
+        }
+
+        private string _productName;
+
+        public string ProductName
+        {
+            get => _productName;
+            set => _productName = value;
+        }
     }
 }
