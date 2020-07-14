f72b9e97ce29a73f5504a5c86c912c462d9a9340
Il Harper <afanyiyu@hotmail.com>
2020-07-14 18:48:56 +0800

    feat: Add ConfigHelper

---
+++ Helpers/ConfigHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Ruminoid.Common.Helpers
+{
+    public static class ConfigHelper<T>
+    {
+        private static string GetConfigFolder(string productName)
+        {
+            string folder = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            @"Ruminoid\Config", productName);
+            Directory.CreateDirectory(folder);
+            return folder;
+        }
+
+        public static T OpenConfig(string productName) =>
+            JsonConvert.DeserializeObject<T>(File.OpenText(Path.Combine(GetConfigFolder(productName), "config.json"))
+                .ReadToEnd());
+
+        public static void SaveConfig(string productName, T config) => File.WriteAllText(
+            Path.Combine(GetConfigFolder(productName), "config.json"), JsonConvert.SerializeObject(config));
+    }
+}
