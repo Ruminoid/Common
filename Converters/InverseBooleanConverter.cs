98dfe176dcfab165afacb26907bf62dd0106cb7e
Il Harper <afanyiyu@hotmail.com>
2020-07-26 21:13:14 +0800

    feat: Add InverseBooleanConverter

---
+++ Converters/InverseBooleanConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Ruminoid.Common.Converters
+{
+    public sealed class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("Target type must be boolean.");
+            return value != null && !(bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("Target type must be boolean.");
+            return value != null && !(bool) value;
+        }
+    }
+}
