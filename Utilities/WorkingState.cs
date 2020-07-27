ef46f849a391303f39abbfa48931f0826bfee4bd
Il Harper <afanyiyu@hotmail.com>
2020-07-27 14:32:51 +0800

    feat: Add WorkingState Enum

---
+++ Utilities/WorkingState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ruminoid.Common.Utilities
+{
+    public enum WorkingState
+    {
+        Unknown = 0,
+        Working = 1,
+        Completed = 2,
+        Failed = 3
+    }
+}
