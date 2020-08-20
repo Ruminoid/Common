b10883f5217be992d2e02c7e9e4a64c83285a6e8
Il Harper <afanyiyu@hotmail.com>
2020-08-20 23:30:26 +0800

    feat: Add CalcFileCrc32 Method

---
+++ Utilities/Algorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Force.Crc32;
+
+namespace Ruminoid.Common.Utilities
+{
+    public static class Algorithm
+    {
+        public static uint CalcFileCrc32(string file) => Crc32Algorithm.Compute(File.ReadAllBytes(file));
+    }
+}
